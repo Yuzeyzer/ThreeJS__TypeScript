d4e84c4e312aa83fa45cc9ef774ef1ba28652d39
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-09-05 04:07:35 +0600

    add the initial scripts

---
+++ src/server/server.ts
@@ -0,0 +1,22 @@
+const port: number = 3000
+
+class App {
+    private server: http.Server
+    private port: number
+
+    constructor(port: number) {
+        this.port = port
+        const app = express()
+        app.use(express.static(path.join(__dirname, '../client')))
+
+        this.server = new http.Server(app);
+    }
+
+    public Start() {
+        this.server.listen(this.port, () => {
+            console.log( `Server listening on port ${this.port}.` )
+        })
+    }
+}
+
+new App(port).Start()
